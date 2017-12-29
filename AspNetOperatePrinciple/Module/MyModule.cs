bd524c3e287e1e98f215f5d5547824d6dc6b922d
Kenny zeng <zengyi_Kenny@outlook.com>
2017-12-29 22:01:30 +0800

    ASP.NET：HTTPModule

---
+++ AspNetOperatePrinciple/Module/MyModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetOperatePrinciple.Module
+{
+    public class MyModule : IHttpModule
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += BeginRequest;
+        }
+
+        private void BeginRequest(object sender, EventArgs e)
+        {
+            HttpApplication application = sender as HttpApplication;
+            application.Context.Response.Write("BeginRequest");
+        }
+    }
+}
